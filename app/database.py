bb9b23c105267cdd5878a94d3c9b1591bc9204e6
sungjin-hong-ktb <sungjin1087@naver.com>
2025-11-29 15:57:34 +0900

    fix: 데이터베이스 주소 수정

--- app/database.py
+++ app/database.py
@@ -1,10 +1,8 @@
-import contextlib
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./community.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
@@ -12,7 +10,6 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-@contextlib.contextmanager
 def get_db():
     db = SessionLocal()
     try:
