a169bf71198249a036ae56f8a1aa102f20c7306b
sungjin-hong-ktb <sungjin1087@naver.com>
2025-11-29 10:05:21 +0900

    feat: password 유효성 검증 추가

--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel, EmailStr, Field
-
+from pydantic import BaseModel, EmailStr, Field, field_validator
+import re
 
 class User(BaseModel):
     id: int
@@ -7,14 +7,30 @@ class User(BaseModel):
     nickname: str
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 class UserCreate(BaseModel):
     email: EmailStr
-    nickname: str
-    password: str
+    nickname: str = Field(..., min_length=1, max_length=10)
+    password: str = Field(..., min_length=8, max_length=20)
+
+    # Password 유효성 검사 (대문자, 소문자, 숫자, 특수문자 각각 최소 1개 포함 여부 검사)
+    @field_validator('password')
+    @classmethod
+    def validate_password(cls, v):
+        # 대문자
+        if not re.search(r'[A-Z]', v):
+            raise ValueError("대문자를 최소 1개 포함해야 합니다.")
+        # 소문자
+        if not re.search(r'[a-z]', v):
+            raise ValueError("소문자를 최소 1개 포함해야 합니다.")
+        # 숫자
+        if not re.search(r'[0-9]', v):
+            raise ValueError("숫자를 최소 1개 포함해야 합니다.")
+        # 특수문자
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', v):
+            raise ValueError("특수문자를 최소 1개 포함해야 합니다.")
+        return v
 
 class UserUpdate(BaseModel):
-    email: EmailStr | None
-    nickname: str | None
-    password: str | None
+    nickname: str | None = Field(..., min_length=1, max_length=10)
