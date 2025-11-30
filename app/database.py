43a55b6fda23860e5bcc27e256f130c5274c4d1d
sungjin-hong-ktb <sungjin1087@naver.com>
2025-11-30 14:17:14 +0900

    feat: 데이터베이스 연결 설정 추가

--- app/database.py
+++ app/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -7,6 +7,14 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
+# SQLite에서 외래키 제약조건 활성화
+@event.listens_for(engine, "connect")
+def set_sqlite_pragma(dbapi_conn, connection_record):
+    cursor = dbapi_conn.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
